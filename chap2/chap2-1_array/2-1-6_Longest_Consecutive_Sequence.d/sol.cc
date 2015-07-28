4bb771eb2f38bfb734c4dd10fe759a548e7bd200
tim <tim@Narwhal.local>
2015-07-28 19:54:34 -0400

    add 2-1-6

---
+++ chap2/chap2-1_array/2-1-6_Longest_Consecutive_Sequence.d/sol.cc
@@ -0,0 +1,28 @@
+#include <unordered_map>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, bool> used;
+        auto max_len = 1;
+        for (auto n : nums) used[n] = false;
+        
+        for (auto n : nums) {
+            auto len = 1;
+            if (used[n]) continue;
+            for (auto j = n + 1; used.find(j) != end(used); ++j) {
+                ++len;
+                used[j] = true;
+            }
+            for (auto j = n - 1; used.find(j) != end(used); --j) {
+                ++len;
+                used[j] = true;
+            }
+            max_len = max(max_len, len);
+        }
+        return max_len;
+    }
+};
