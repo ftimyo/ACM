533dc26981a48bb8045b4a597e1301db0ab4e28d
tim <tim@Narwhal.local>
2015-08-04 16:58:28 -0400

    format update for 2-1-13

--- chap2/chap2-1_array/2-1-13_Permutation_Sequence.d/sol.cc
+++ chap2/chap2-1_array/2-1-13_Permutation_Sequence.d/sol.cc
@@ -4,12 +4,13 @@
 #include <vector>
 #include <string>
 using namespace std;
+
 class Solution {
 public:
 	string getPermutation(int n, int k) {
-		string s{};
-		vector<bool> ds(10, false);
 		int f[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+		vector<bool> ds(10, false);
+		string s{};
 
 		/* calculate factorials
 		vector<int> f(10, 1);
@@ -17,14 +18,14 @@ public:
 		partial_sum(next(begin(f)), end(f), next(begin(f)), multiplies<int>());
 		*/
 
-		auto q = k - 1 /*cantor encoding starts from 0*/, r = 0;
+		auto r = k - 1;	/*cantor encoding starts from 0*/
 		for (auto i = n - 1; i >= 0; --i) {
-			auto dv = div(q, f[i]);
-			q = dv.rem, r = dv.quot;
+			auto dv = div(r, f[i]);
+			auto q = dv.quot; r = dv.rem;
 			auto d = 1;
 			for (; d <= n; ++d) {
 				if (ds[d]) continue;
-				if (r-- == 0) break;
+				if (q-- == 0) break;
 			}
 			ds[d] = true;
 			s += d + '0';
