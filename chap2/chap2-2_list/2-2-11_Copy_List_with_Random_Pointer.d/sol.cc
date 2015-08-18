835ca899f8698814920696d81b42867742a0bddc
tim <tim@Narwhal.stny.rr.com>
2015-08-18 00:12:44 -0400

    add solution to 2-2-11 Copy List with Random Pointer

---
+++ chap2/chap2-2_list/2-2-11_Copy_List_with_Random_Pointer.d/sol.cc
@@ -0,0 +1,33 @@
+#include <cstring>
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *	 int label;
+ *	 RandomListNode *next, *random;
+ *	 RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+class Solution {
+public:
+	RandomListNode *copyRandomList(RandomListNode *head) {
+		using H = RandomListNode;
+		if (!head) return head;
+		for (auto p = head; p != nullptr; p = p->next->next)
+			p->next = new H{*p};
+		for (auto p = head; p != nullptr; p = p->next->next)
+			if (p->next->random) p->next->random = p->next->random->next;
+
+		H h{0};
+		for (auto p = head, q = &h; p != nullptr; p = p->next) {
+			q->next = p->next;
+			q = q->next;
+			p->next = q->next;
+		}
+		return h.next;
+	}
+};
