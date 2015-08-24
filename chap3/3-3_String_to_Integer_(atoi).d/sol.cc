a6c102346ebc47288e90f74136b20f82abe555f7
tim <tim@Narwhal.stny.rr.com>
2015-08-24 12:46:45 -0400

    update solution to 3-3

--- chap3/3-3_String_to_Integer_(atoi).d/sol.cc
+++ chap3/3-3_String_to_Integer_(atoi).d/sol.cc
@@ -6,24 +6,28 @@ class Solution {
 public:
 	int myAtoi(string str) {
 		int sn = 1, base = 10;
-		/*remove white space*/
+		auto high = INT_MAX / base, low = INT_MAX % base;
+
+		/*remove white spaces*/
 		str.erase(begin(str), find_if_not(begin(str), end(str), ::isspace));
 		if (str.empty()) return 0;
-		/*get sign prefix p*/
+
+		/*extract sign*/
 		auto h = find_if(begin(str), end(str), ::isdigit);
-		string p {begin(str), h}; str.erase(begin(str), h);
-		if (str.empty()) return 0;
-		if (p.length() == 1) {if (p[0] == '-') sn = -1; else if (p[0] != '+') return 0;}
-		else if (!p.empty()) return 0;
-		/*remove trailing non-digit chars*/
+		string p{begin(str), h}; str.erase(begin(str), h);
 		str.erase(find_if_not(begin(str), end(str), ::isdigit), end(str));
 		if (str.empty()) return 0;
+		if (p.length() == 1) {if (p.front() == '-') sn = -1; else if (p.front() != '+') return 0;}
+		else if (!p.empty()) return 0;
 
+		/*extract number*/
 		int sum = 0;
 		for (auto c : str) {
 			c -= '0';
-			if (sum > INT_MAX / 10 || (sum == INT_MAX / 10 && c > INT_MAX % 10))
+
+			if (sum > high || (sum == high && c > low))
 				return sn > 0 ? INT_MAX : INT_MIN;
+
 			sum = sum * base + c;
 		}
 		return sum * sn;
