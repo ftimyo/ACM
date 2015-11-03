fd8fa4e2a371f3b719d94078ba1bc7b00f1bfbcf
tim <tim@Narwhal.stny.rr.com>
2015-11-03 10:48:21 -0500

    update solution for 2-1-03

--- chap2/chap2-1_array/2-1-03_Search_in_Rotated_Sorted_Array.d/sol.cc
+++ chap2/chap2-1_array/2-1-03_Search_in_Rotated_Sorted_Array.d/sol.cc
@@ -1,21 +1,18 @@
 #include <vector>
 using namespace std;
-
 class Solution {
 public:
 	int search(vector<int>& nums, int target) {
-		int first = 0, last = nums.size();
-		while (first != last) {
-			auto mid = (first + last) / 2;
+		int i = 0, j = nums.size();
+		while (i != j) {
+			auto mid = (i + j) / 2;
 			if (nums[mid] == target) return mid;
-			if (nums[first] < nums[mid]) {
-				if (nums[first] <= target && target < nums[mid]) last = mid;
-				else first = mid + 1;
-			} else if (nums[first] == nums[mid]) {
-				++first;
+			if (nums[i] < nums[mid]) {
+				if (nums[i] <= target && target < nums[mid]) j = mid;
+				else i = mid + 1;
 			} else {
-				if (nums[mid] < target && target <= nums[last - 1]) first = mid + 1;
-				else last = mid;
+				if (nums[mid] < target && target <= nums[j - 1]) i = mid + 1;
+				else j = mid;
 			}
 		}
 		return -1;
