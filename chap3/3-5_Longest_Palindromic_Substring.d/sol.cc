2d10dc6376772c70c8e25cb0e6e4b557da88a6c1
tim <tim@Narwhal.stny.rr.com>
2016-11-11 12:05:16 -0500

    update longest palindrome substring using DP

--- chap3/3-5_Longest_Palindromic_Substring.d/sol.cc
+++ chap3/3-5_Longest_Palindromic_Substring.d/sol.cc
@@ -3,19 +3,22 @@ using namespace std;
 class Solution {
 public:
 	string longestPalindrome(string s) {
-		auto len = s.length();
-		if (len <= 1) return s;
-
-		int max_len = 0, max_start = 0;
-		bool f[len][len];
-
-		for (size_t l = 0; l < len; ++l) {
-			for (size_t i = 0; i < len - l; ++i) {
-				auto j = i + l;
-				f[i][j] = (j == i) || (s[i] == s[j] && (l == 1 || f[i+1][j-1]));
-				if (f[i][j]) {max_start = i; max_len = l;}
+		auto str = s.c_str();
+		auto n = s.size();
+		decltype(n) maxlen = 1;
+		int idx = 0;
+		bool p[n][n];
+		fill_n(*p,n*n,false);
+		for (decltype(n) i = 0; i < n; ++i) {
+			p[i][i] = true;
+			for (decltype(n) j = 0; j < i; ++j) {
+				p[j][i] = (str[i] == str[j]) && (i - j < 2 || p[j+1][i-1]);
+				if (p[j][i] && i - j + 1 > maxlen) {
+					maxlen = i - j + 1;
+					idx = j;
+				}
 			}
 		}
-		return s.substr(max_start, max_len+1);
+		return s.substr(idx,maxlen);
 	}
 };
